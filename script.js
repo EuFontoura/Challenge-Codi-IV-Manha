be9ed27732e7f8cfbe672d810dc341c55168e53d
juliabeccari <julia,beccari@estudante.ufjf.br>
2024-03-26 15:54:13 -0300

    Adicionando estilizacao das informacoes

--- script.js
+++ script.js
@@ -1,96 +1,215 @@
-// Olá, bem vindo ao código da parte lógica de nosso projeto!
-
-// Este é o rascunho inicial para o código do app de finanças da equipe da Manhã do CODI Challenge 2024.
-// A ideia inicial é obter classes para instanciar cada registro de despesa como um objeto com as atribuições definidas pelo usuário.
-// Também será criada uma classe para Categorias, permitindo que seja implementado futuramente uma forma de adicionar categorias.
-
-//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-// PARTE LOGICA
-
-// Declarar as variáveis de entry seguindo o exemplo apresentado no Figma como protótipo:
-
-let valor
-let data
-let categoria
-let descricao
-
-// Declarar uma classe para os objetos de cada despesa.
-
 class Despesa {
-    constructor(valor, data, descricao) {
+    constructor(valor, moeda, data, descricao, categoria) {
         this.valor = valor
+        this.moeda = moeda
         this.data = data
         this.descricao = descricao
+        this.categoria = categoria
     }
 }
 
-// Declarar uma classe para separar as despesas em categorias.
-// A classe possui [] que funciona como um container vazio que irá receber as instâncias de despesa.
-
 class Categoria {
-    constructor(tipo) {
+    constructor(tipo, nome) {
+        this.nome = nome
         this.tipo = tipo
         this.despesas = []
     }
-}
-
-// Declaração das categorias pré definidas seguindo o exemplo do protótipo do Figma.
-
-let moradia = new Categoria("Moradia")
-let alimentacao = new Categoria("Alimentação")
-let saudebeleza = new Categoria("Saúde & Beleza")
-let vestuario = new Categoria("Vestuário")
-
-//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-// PARTE VISUAL E INTERATIVA
-
-// FUNCOES
-// Função para manipular as tabs de categorias.
-const tabClicked = (tab) => {
-
     
-    const contents = document.querySelectorAll('.content')
+    add_despesa(despesa) {
+        this.despesas.push(despesa)
+    }
+
+    somar_despesas() {
+        let total = 0
+        for (let despesa of this.despesas) {
+            total += despesa.valor
+        }
+        return total
+    }
+
+    listar_despesas() {
+        console.log(`Despesas na categoria ${this.tipo}:`)
+        for (let despesa of this.despesas) {
+            console.log(`Valor: ${despesa.valor}, Data: ${despesa.data}, Descrição: ${despesa.descricao}`)
+        }
+    }
+}
+
+function criar_categoria(nomesCategorias) {
+    const categorias = []
+
+    nomesCategorias.forEach(nome => {
+        const tipoFormatado = formatar_texto(nome)
+        const novaCategoria = new Categoria(tipoFormatado, nome)
+        categorias.push(novaCategoria)
+    })
+
+    return categorias
+}
+
+
+// adiciona despesa no array vazio de cada categoria
+
+
+function despesa_no_array(despesa, categorias) {
+    const categoriaDespesa = despesa.categoria.toLowerCase()
+
+    const categoriaTodos = categorias.find(categoria => categoria.tipo === "todos") || new Categoria("todos", "Todos")
+    categoriaTodos.add_despesa(despesa)
+
+    const categoriaEncontrada = categorias.find(categoria => categoria.tipo === categoriaDespesa)
+    if (categoriaEncontrada) {
+        categoriaEncontrada.add_despesa(despesa)
+
+        localStorage.setItem('categorias', JSON.stringify(categorias))
+    } else {
+        console.log("Categoria não encontrada.")
+    }
+}
+
+function add_btn_categoria(categorias) {
+    const divButtons = document.querySelector('.divButtons')
+    const divContents = document.querySelector('.divContents')
+
+    if (divButtons && divContents) {
+        
+        divButtons.innerHTML = ''
+
+        const divBack = document.createElement('div')
+        divBack.classList.add('backBtn')
+        divButtons.appendChild(divBack)
+
+        categorias.forEach(categoria => {
+            const botao = document.createElement('button')
+            botao.textContent = categoria.nome
+            botao.classList.add('tabBtn')
+            botao.setAttribute('id', formatar_texto(categoria.tipo))
+            divButtons.appendChild(botao)
+
+            botao.addEventListener('click', function() {
+                divContents.innerHTML = ''
+
+                const listaDespesas = document.createElement('ul')
+                listaDespesas.classList.add('listaDespesas')
+
+                categoria.despesas.forEach(despesa => {
+                    const itemLista = document.createElement('li')
+                    itemLista.classList.add('itemListaDespesa')
+                    itemLista.textContent = `${despesa.data} - ${despesa.categoria} - ${despesa.moeda} - ${despesa.descricao}`
+                    listaDespesas.appendChild(itemLista)
+                })
+                divContents.appendChild(listaDespesas)
+            })
+        })
+    }
+}
+
+function formatar_texto(texto) {
+    return texto.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/\s+/g, "_")
+}
+
+function formatar_dinheiro(input) {
+    let num = input.value.replace(/\D/g, '')
+    input.value = (num / 100).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+}
+
+const nomesCategorias = ["Todos", "Moradia", "Alimentação", "Saúde e Beleza", "Vestuário", "Outros"]
+const categorias = criar_categoria(nomesCategorias)
+
+
+// ===============================================================================================================================
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    let categorias = JSON.parse(localStorage.getItem('categorias'))
+
+    if (categorias) {
+
+        categorias = categorias.map(categoria => {
+            const categoriaRestaurada = Object.assign(new Categoria(), categoria)
+            categoriaRestaurada.despesas = categoria.despesas.map(despesa => new Despesa(despesa.valor, despesa.moeda, despesa.data, despesa.descricao, despesa.categoria))
+            return categoriaRestaurada
+        })
+
+    } else {
+
+        categorias = criar_categoria(nomesCategorias)
+        localStorage.setItem('categorias', JSON.stringify(categorias))
+    }
+
+    add_btn_categoria(categorias)
     
-    // Primeiro removemos a classe show de todos os conteúdos
-    contents.forEach(content => content.classList.remove('show'))
+    document.getElementById("register-button").addEventListener("click", function() {
+        document.querySelector(".form").style.display = "flex"
+    })
 
-    // Adicionamos a classe show apenas para o conteúdo que tem o mesmo content-id que o botão de tab clicado
-    const contentId = tab.getAttribute('content-id')
-    const content = document.getElementById(contentId) //esta funcao pega o id com aquele nome e me retorna o elemento (pode ser div, h1, oq seja)
-    content.classList.add('show') //adiciona a classe show para o conteudo que tem o mesmo id que o botao clicado
+    const form = document.getElementById('formInfo')
+    const closeButton = document.getElementById('close')
 
-}
+    document.querySelector(".close").addEventListener("click", function() {
+        document.querySelector(".form").style.display = "none"
+    })
 
+    document.getElementById("cancel").addEventListener("click", function() {
+        document.querySelector(".form").style.display = "none"
+    })
 
-// EVENTOS
-// Primeiro pegamos todos os botoes de tab que iremos manipular
-const tabs = document.querySelectorAll('.tab-btn')
+    closeButton.addEventListener('click', function() {
+        form.reset()
+    })
 
-// Adicionamos um evento de click para cada botão de tab e chamamos uma funcao para manipular o conteudo
-tabs.forEach(tab => tab.addEventListener('click', () => tabClicked(tab)))
+    document.getElementById('submit').addEventListener('click', function(event) {
+        event.preventDefault();
+        
+        const moeda = document.getElementById("money").value;
+        const valor = parseFloat(moeda.replace(/[^\d,]/g, '').replace(',', '.'));
+        const data = document.getElementById("date").value;
+        const descricao = document.getElementById("description").value;
+        const categoria = document.getElementById("category").value;
+        
+        const novaDespesa = new Despesa(valor, moeda, data, descricao, categoria);
+        
+        console.log("Despesa criada:", novaDespesa);
+        console.log("Botão clicado: submit");
+        
+        despesa_no_array(novaDespesa, categorias);
+        
+        console.log(categorias);
+    
+        // Procurar pelo botão da categoria correspondente
+        const botaoCategoria = document.getElementById(formatar_texto(categoria));
+        if (botaoCategoria) {
+            botaoCategoria.click(); // Clicar no botão da categoria
+        } else {
+            console.log("Botão de categoria correspondente não encontrado.");
+        }
+        form.reset()
+    });
+    
+    let currentIndex = 0
 
-// Função para formatar o valor inserido do form para reais
-function formatarDinheiro(input) {
-    let num = input.value.replace(/\D/g, '');
-    input.value = (num / 100).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-}
+    document.getElementById('left').addEventListener('click', function() {
+        console.log("Botão clicado: left")
+ 
+        currentIndex > 0 ? currentIndex--: currentIndex = categorias.length - 1
+        console.log("Índice atual:", currentIndex)
+    
+        const categoriaAtual = categorias[currentIndex].tipo
+        const botaoCategoria = document.getElementById(formatar_texto(categoriaAtual))
+        botaoCategoria ? botaoCategoria.click() : console.log("Botão de categoria correspondente não encontrado.")
+        
+    })
+    
+    document.getElementById('right').addEventListener('click', function() {
+        console.log("Botão clicado: right")
 
-// Manipulando o pop-up de registro de despesas
-document.getElementById("register-button").addEventListener("click", function() {
-    document.querySelector(".form").style.display = "flex";
-})
+        currentIndex < categorias.length - 1 ? currentIndex++ : currentIndex = 0
+        console.log("Índice atual:", currentIndex)
+    
+        const categoriaAtual = categorias[currentIndex].tipo
+        const botaoCategoria = document.getElementById(formatar_texto(categoriaAtual))
+        botaoCategoria ? botaoCategoria.click() : console.log("Botão de categoria correspondente não encontrado.")
+    })
 
-document.querySelector(".close").addEventListener("click", function() {
-    document.querySelector(".form").style.display = "none";
-})
-
-document.getElementById("cancel").addEventListener("click", function() {
-    document.querySelector(".form").style.display = "none";
-})
-
-const form = document.getElementById('formInfo');
-const closeButton = document.getElementById('close');
-
-closeButton.addEventListener('click', function() {
-    form.reset();
 })
